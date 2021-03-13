b3f96314fab3081c854c45427e28bcd69e9c9f93
shahirg <shahir13579@gmail.com>
2021-03-13 00:11:48 -0500

    got V2 kinda working

--- project1/problem2/main.c
+++ project1/problem2/main.c
@@ -5,57 +5,132 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
 
-void oneProcess(int numlines, int numKeys, char* fileName){
-    
-    // char buf[128];
-    // printf("%s",&fileName);
-    // int fd;
-    // fd = open(&fileName, O_RDONLY);
-    // if(fd == -1){
-    //     printf("Failed to open %s\n", fileName);
-    //     exit(1);//
-    // }
-    // read(fd, buf, sizeof(buf));
-    // //for(int i = 0)
 
-    // printf("%s\n",buf);
-    int num, keysFound=0,linesread = 0, max = -10,total = 0;
-    double avg;
-    FILE * fp;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    fp = fopen(fileName, "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
-
-    while ((read = getline(&line, &len, fp)) != -1) {
-        //printf("Retrieved line of length %zu:\n", read);
-        linesread++;
-        num = atoi(line);
-        total += num;
-        max = num > max ? num:max;
-        if(num == -10){
-            keysFound++;
-            printf("HI I am process %d and I found a hidden key in position A[%d]\n",getppid(),linesread);
+void oneProcess(int numbers[], int L, int H){
+    int found = 0;
+    for(int i = 0; i < L; i++){
+        if(numbers[i]== -10){
+            printf("Hi I am process %d and I found a hidden key at A[%d]\n",getpid(),i);
+            found++;
         }
-        if(keysFound >=numKeys)
+        if(found ==H)
             break;
-        //printf("%s", line);
+        
     }
-    avg = total/linesread;
-    fclose(fp);
-    if (line)
-        free(line);
-    printf("Max= %d Avg= %f\n",max,avg);
-    exit(EXIT_SUCCESS);
-
-
 }
-void vesrionTwo(int numbers[],int L, int H){
 
+int searchBlock(int numbers[], int start, int end, int L , int H, int* foundKeys){
+    int foundCount = 0;
+
+	size_t i;
+
+	for (i = start; i < L && i < end; i++)
+	{
+		// Key check.
+		if (numbers[i] == -10)
+		{
+			foundKeys[foundCount++] = i;
+			printf("I am process %d and I found the key at %d\n", getpid(), foundKeys[foundCount - 1]);
+		}
+		// All found check.
+		if (foundCount == H){
+			break;
+		}
+	}
+
+
+
+	// Return number of keys found.
+	return foundCount;
+}
+
+void vesrionTwo(int numbers[],int L, int H){
+    
+    int numFound = 0,searchResult,j =0;
+    int splitSize = L/H;
+    
+    int *foundIndexes = (int*)malloc(H*sizeof(int));
+    int *keyIndexes = (int*)malloc(H*sizeof(int));
+    pid_t p, root,start,end;
+    int pipefd[2];
+    if((pipe(pipefd)) == -1){
+        printf("Pipe creation failure");
+        exit(EXIT_FAILURE);
+    }
+
+    root = getpid();
+
+    for(int i =0; i < H;i++){
+        start = splitSize*i;
+        end = (i+1) * splitSize;
+        if((p = fork()) < 0){
+            printf("Fork failed\n");
+            exit(1);
+        }
+        else if(p == 0){
+            printf("I am process %d, my parent is %d\n", getpid(), getppid());
+            if(i == H){
+                
+				// Next send number of found indexes (should be 0).
+				write(pipefd[1], &numFound, sizeof(int));
+				// Now, pipe each found index (should be none).
+				for (j = 0; j < numFound; j++){
+					write(pipefd[1], &(keyIndexes[j]), sizeof(int));
+				}
+            }
+        }
+        else if(p > 0){
+			// Just search, then break.
+			searchResult = searchBlock(numbers,start,end,L,H,keyIndexes);
+
+			for (j = 0; j < searchResult; j++)
+			{
+				foundIndexes[j] = keyIndexes[j];
+				numFound++;
+			}
+
+			// Wait for the child.
+			waitpid(p, NULL, 0);
+
+			// Read the data from the child's search, and compare it to this process's search.
+			int readFoundCount, readFoundIndex;
+
+			// Next read number of found indexes.
+			read(pipefd[0], &readFoundCount, sizeof(int));
+			// Now, read each found index.
+			for (j = 0; j < readFoundCount; j++)
+			{
+				read(pipefd[0], &readFoundIndex, sizeof(int));
+				foundIndexes[numFound++] = readFoundIndex;
+				//printf("Reading index: %d\n", readFoundIndex);
+			}
+
+	
+
+			// If this process is NOT the starting process, pipe.
+			if (!(getpid() == root))
+			{
+				// Pipe the data from this search to the parent of this process.
+				// First send max.
+
+				write(pipefd[1], &numFound, sizeof(int));
+				// Now, pipe each found index.
+				for (j = 0; j < numFound; j++)
+				{
+					write(pipefd[1], &(foundIndexes[j]), sizeof(int));
+					//printf("Writing index: %d\n", foundIndexes[j]);
+				}
+			}
+            break;
+
+        }
+    }
+    free(foundIndexes);
+	free(keyIndexes);
+    exit(EXIT_SUCCESS);
 }
 
 
@@ -65,19 +140,17 @@ int main(int argc, char **argv){
     //argv[3] = filename
     //argv[4] = mode 
     if(argc != 5){
-        return(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
 
-    int L = atoi(argv[1]);
-    int H = atoi(argv[2]);
-    int mode = atoi(argv[4]);
+    int L = atoi(argv[1]),H = atoi(argv[2]),mode = atoi(argv[4]);
     //populate array
     int numbers[L];
     FILE * fp;
     char * line = NULL;
     size_t len = 0;
     ssize_t read;
-    int i = 0,num;
+    int i = 0, num, max = -10, total=0;
     fp = fopen(argv[3], "r");
     if (fp == NULL)
         exit(EXIT_FAILURE);
@@ -85,17 +158,23 @@ int main(int argc, char **argv){
     while ((read = getline(&line, &len, fp)) != -1) {
         //printf("Retrieved line of length %zu:\n", read);
         num = atoi(line);
-        printf("num = %d\n",num);
         numbers[i]= num;
-        printf("num = %d\n",num);
+        total +=num;
+        max = num > max ? num: max;
         i++;
     }
+    printf("Max = %d, Average = %f\n", max, (total*1.0/L));
     close(fp);
-    for(i=0; i<L; i++){
-        printf("%d\n",numbers[i]);
-    }
-    //oneProcess(lines,argv[2],numKeys);
+
+    if(mode == 1)
+        oneProcess(numbers,L,H);
+    if(mode == 2)
+        vesrionTwo(numbers,L,H);
     return 0;
     //
 }
+
+
+
+
 
